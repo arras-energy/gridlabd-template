4b6f9823379a93fe6ca0b83235ffa3b8c177aa56
jewelnewman <newmanjewel2@gmail.com>
2021-06-28 19:40:33 -0600

    Edited initiation fnc draft in .py file and finished TOU tariff class

--- US/CA/SLAC/tariff_design/tariff_design.py
+++ US/CA/SLAC/tariff_design/tariff_design.py
@@ -1,10 +1,70 @@
 import gridlabd
+import csv
+import pandas 
+
+# IMPROVE THIS TO REMOVE HARD CODING?
+data_list = ['dgrules','mincharge',
+ 'energyratestructure/period0/tier0max',
+ 'energyratestructure/period0/tier0rate',
+ 'energyratestructure/period0/tier1rate',
+ 'energyratestructure/period1/tier0max',
+ 'energyratestructure/period1/tier0rate',
+ 'energyratestructure/period1/tier1rate',
+ 'energyratestructure/period2/tier0max',
+ 'energyratestructure/period2/tier0rate',
+ 'energyratestructure/period2/tier1rate',
+ 'energyratestructure/period3/tier0max',
+ 'energyratestructure/period3/tier0rate',
+ 'energyratestructure/period3/tier1rate',
+ 'energyratestructure/period4/tier0max',
+ 'energyratestructure/period4/tier0rate',
+ 'energyratestructure/period4/tier1rate',
+ 'energyweekdayschedule',
+ 'energyweekendschedule']
 
 def on_init(t):
+	i=0
+	global data_list
+
+	# CHANGE TO USE REQUEST & ZIPFILE PY LIBRARY
+	# import in tariff data from API
+	'''
+	import os
+	if not os.path.exists('usurdb.csv'):
+    	import os
+    	os.system('curl https://openei.org/apps/USURDB/download/usurdb.csv.gz | gunzip > usurdb.csv')
+	'''	
+	pandas.set_option("max_rows",None)
+	pandas.set_option("max_columns",None)
+	data = pandas.read_csv("../../../../../usurdb.csv",low_memory=False)
+
 
 	# read in csv file
-	# parse database
-	# define gridlabd objects
+	with open("tariff_library_config.csv") as fp:
+		reader = csv.reader(fp, delimiter=",")
+		next(reader)
+		tariff_input = [row for row in reader]
+	
+	for t in tariff_input:
 
+		# get inputs from csv
+		utility_name = data_read[i][0]
+		sector_type = data_read[i][1]
+		name = data_read[i][2]
+		region = data_read[i][4]
+		i = i + 1
+
+		# parse database
+		utility = data[data.utility==utility_name]
+    	utility_active = utility[utility["enddate"].isna()]
+   		mask = utility_active["name"].str.contains(name, regex=True, case=False) && utility_active["name"].str.contains(region, regex=False)
+   		tariff_data = utility_active[mask].reset_index()
+
+		# define gridlabd objects
+		for column in data_list:
+    		if tariff_data[column].isnull().values.any() == False:
+       			gridlabd.setvalue(name, column, tariff_data.at[0,column])
+    	else:
+        	continue
 
 	return True
