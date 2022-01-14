a59032e05baf5b279d9f00978e4a3dfe13357cd4
Johnson Hsiung <johnson.hsiung@sjsu.edu>
2022-01-14 15:05:06 -0800

    testing github repo and openfido connection

--- openfido.sh
+++ openfido.sh
@@ -1,1 +1,13 @@
-    #!/bin/sh
+#!/bin/sh
+
+
+# nounset: undefined variable outputs error message, and forces an exit
+set -u
+# errexit: abort script at first error
+set -e
+# print command to stdout before executing it:
+set -x
+
+echo "hi"
+
+exit 0
