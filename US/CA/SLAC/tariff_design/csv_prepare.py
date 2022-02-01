38c0b8e12368c451f46173321cfbf4cb092343a5
Johnson Hsiung <johnson.hsiung@sjsu.edu>
2022-02-01 14:56:28 -0800

    testing openfido

--- US/CA/SLAC/tariff_design/csv_prepare.py
+++ US/CA/SLAC/tariff_design/csv_prepare.py
@@ -79,8 +79,12 @@ def generate_tariff_index(df, df_tariff_index):
 
     # Query one at a time to better pin point error 
     df_tariff_index.query('utility == @tariff_utility', inplace = True)
+    print(df_tariff_index.to_string())
     df_tariff_index.query('name == @tariff_name', inplace = True)
-    df_tariff_index.query('region == @tariff_region', inplace = True)
+    print(df_tariff_index.to_string())
+    df_tariff_index.query('region == @tariff_region', inplace = True)    
+    print(df_tariff_index.to_string())
+
 
 
     # These values are currently the same for all provided rows  
