b3cb21df7fb99e5c7233f284b25a15b26960eb77
Johnson Hsiung <johnson.hsiung@sjsu.edu>
2022-02-01 15:33:56 -0800

    testing openfido

--- US/CA/SLAC/tariff_design/csv_prepare.py
+++ US/CA/SLAC/tariff_design/csv_prepare.py
@@ -75,17 +75,17 @@ def generate_tariff_index(df, df_tariff_index):
             print(tariff_inclining_block_rate)
 
     # In case of white spaces
-    df_tariff_index.columns = [column.replace(" ", "_") for column in df_tariff_index.columns]
+    df_tariff_index.columns = [column.replace(" ", "") for column in df_tariff_index.columns]
 
     # Query one at a time to better pin point error 
     print(df_tariff_index.to_string())
     df_tariff_index.query('utility == @tariff_utility', inplace = True)
     print(df_tariff_index.to_string())
 
-    df_tariff_index.query('_region == @tariff_region', inplace = True) 
+    df_tariff_index.query('region == @tariff_region', inplace = True) 
 
    
-    #df_tariff_index.query(' _name == @tariff_name', inplace = True)
+    #df_tariff_index.query('name == @tariff_name', inplace = True)
     print(df_tariff_index.to_string())
     
 
@@ -94,9 +94,9 @@ def generate_tariff_index(df, df_tariff_index):
 
 
     # These values are currently the same for all provided rows  
-    #df_tariff_index.query(' _sector == @tariff_sector', inplace = True)
-    #df_tariff_index.query(' _type == @tariff_type', inplace = True)
-    #df_tariff_index.query(f' _INCLINING_BLOCK_RATE == {tariff_inclining_block_rate}', inplace = True)
+    #df_tariff_index.query('sector == @tariff_sector', inplace = True)
+    #df_tariff_index.query('type == @tariff_type', inplace = True)
+    #df_tariff_index.query(f'INCLINING_BLOCK_RATE == {tariff_inclining_block_rate}', inplace = True)
     #df_tariff_index.query('{sector == @tariff_sector', inplace = True)
 
     # TODO: Currently assumes one match 
