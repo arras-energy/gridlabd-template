fe26f141b70609db217efbab9e8a519e5f0ff13d
Johnson Hsiung <johnson.hsiung@sjsu.edu>
2022-02-12 15:53:28 -0800

    added date time parsing and housekeeping and comments

--- US/CA/SLAC/tariff_design/csv_prepare.py
+++ US/CA/SLAC/tariff_design/csv_prepare.py
@@ -2,59 +2,86 @@ import csv
 import pandas as pd 
 import re
 import gridlabd 
+import datetime 
+from dateutil import parser 
 
-# The column names for config.csv 
-df_column_one_name = "Header"
-df_column_two_name = "Value"
-config_file = "config.csv"
-tariff_index_file = "tariff_library_config.csv"
-# Parses weather station value. Checks for no spaces, capitalization, 
+
+df_column_one_name = "Header" # config.csv column one name 
+df_column_two_name = "Value" # config.csv column two name 
+config_file = "config.csv" # name of config file 
+tariff_index_file = "tariff_library_config.csv" # name of tariff config file 
+
+
 def parse_weather(value, row, df,tariff_index_file):
+    """ Currently needs to be implemented. 
+    """
+
     print("hi")
 def parse_time(value, row, df,tariff_index_file):
+    """ Parses time value in ISO8601 or YYYY-MM-DD DD:HH:MM TZN. Latter format will throw a warning.
+
+    """
+    print(parser.parse(value))
     if re.match("^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]) ([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9] [A-Z]{3}$", value) == None:
         print("bad time")
-# Matches exactly for now 
+
 def parse_time_zone(value, row, df,tariff_index_file):
+    """ Parses time zone value with any three capital letters followed by a "+", a number, and any three more capital letters. 
+    
+    Example: EST+5EDT
+    """
     if re.match("^[A-Z]{3}\+[1-9][A-Z]{3}$", value) == None:
         print("bad")
-# Parses model name value. Makes sure there is .glm at the end. 
 def parse_model_name(value, row, df,tariff_index_file):
+    """ Parses name of model. Appends .glm if not present at end of string.
+    """
     if not value.endswith('.glm'):
         gridlabd.warning("Model name should end with .glm. Adding .glm...")
-        df.at[row, df_column_two_name] = value.strip() + ".glm"
-# Parses output name value. Makes sure there is .csv at the end. 
+        df.at[row, df_column_two_name] = value + ".glm"
 def parse_output_name(value, row, df,tariff_index_file):
+    """ Parses name of output file. Appends .csv if not present at the end of string. 
+    """
     if not value.endswith('.csv'):
         gridlabd.warning("Output name should end with .csv Adding .csv...")
-        df.at[row, df_column_two_name] = value.strip() + ".csv"
-# could do levenshtein distance
+        df.at[row, df_column_two_name] = value + ".csv"
+
 def parse_tariff_utility(value, row, df,tariff_index_file):
+    """ TODO: Levenshtein distance
+    """
     unique_utility = tariff_index_file.utility.unique()
     if not value in unique_utility:
         gridlabd.warning(f"{value} not recognized. Will attempt to generate tariff configuration.\nOnly {unique_utility} are accepted.")
 def parse_tariff_sector(value, row, df,tariff_index_file):
+    """ Currently not needed and is just a function stub.
+    """
     raise NotImplementedError
-# Only takes in a few values. 
 def parse_tariff_name(value, row, df,tariff_index_file):
+    """ Matches value with names in tariff configuration file. 
+    """
     unique_tariff_name = tariff_index_file.name.unique()
     if not value in unique_tariff_name:
         gridlabd.warning(f"{value} not recognized. Will attempt to generate tariff configuration\nOnly {unique_tariff_name} are accepted.")
 def parse_tariff_type(value, row, df,tariff_index_file):
+    """ Currently not needed and is just a function stub.
+    """
     raise NotImplementedError
 # Only takes in a few values. 
 def parse_tariff_region(value, row, df,tariff_index_file):
+    """ Matches value with regions in tariff configuration file. 
+    """
     unique_tariff_region = tariff_index_file.region.unique()
     if not value in unique_tariff_region:
         gridlabd.warning(f"{value} not recognized. Will attempt to generate tariff configuration\nOnly {unique_tariff_region} are accepted.")
 def parse_tariff_inclining_block_rate(value, row, df,tariff_index_file): 
     raise NotImplementedError
 def default(value, row, df,tariff_index_file): 
-    gridlabd.warning(f"{df_column_one_name} value -not supported")
+    """ Handles unsupported values 
+    """
+    gridlabd.warning(f"({df.at[row, df_column_one_name]}, {value}) on row {row} not supported")
 
 def parse_csv_values(df,tariff_index_file):
-    # Error checking done here 
-    # Check for header values 
+    """ Loops through df and calls parsing functions based on its corresponding label. 
+    """
     switcher = {
     "WEATHER_STATION": parse_weather,
     "STARTTIME": parse_time,
@@ -72,17 +99,20 @@ def parse_csv_values(df,tariff_index_file):
     for index, row in df.iterrows():
         df.at[index, df_column_two_name] = row[df_column_two_name].strip()
         try:
-            switcher.get(row[df_column_one_name], default)(row[df_column_two_name], index, df,tariff_index_file)
+            switcher.get(row[df_column_one_name], default)(row[df_column_two_name], index, df,tariff_index_file) # second parethesis provides arguments to functions
         except ValueError as e:
             raise
 def is_column_names_valid(df):
+    """ Checks the first row of df to make sure column 1 is "Header" and column 2 is "Value"
+    """
     if len(df.columns) != 2 or df.columns[0] != df_column_one_name or df.columns[1] != df_column_two_name:
         raise ValueError(f"{config_file} column headers must be 'Header' and 'Value'")
     
 
 
 def generate_tariff_index(df, df_tariff_index):
-    # Loop through df again and find the tariff values. Match them with df_tariff_index
+    """ Generates tariff index (row number in tariff_config) based on matching values of df (config.csv) and df_tariff_index
+    """
     tariff_utility = ""
     tariff_sector = ""
     tariff_name = ""
@@ -106,7 +136,7 @@ def generate_tariff_index(df, df_tariff_index):
     # In case of white spaces
     df_tariff_index.columns = [column.replace(" ", "") for column in df_tariff_index.columns]
     df_copy = df_tariff_index.copy(deep=True)
-    # Query one at a time to better pin point error 
+    # Query one at a time and preserves last dataframe that had values. 
     if (tariff_utility != ""):
         df_copy = df_tariff_index.query('utility == @tariff_utility', inplace = False)
         if (len(df_copy) > 0):
@@ -120,6 +150,7 @@ def generate_tariff_index(df, df_tariff_index):
         if (len(df_copy) > 0):
             df_tariff_index = df_copy.copy(deep=False)
 
+    # Testing with indexes
     #df_tariff_index['row_num'] = df_tariff_index.index;
     #df_tariff_index.set_index(["utility","name", "region"],inplace=True)
     #df_tariff_index = df_tariff_index.loc[tariff_utility][["row_num"]]
@@ -153,6 +184,8 @@ def generate_tariff_index(df, df_tariff_index):
     
 
 def add_tariff_index_row(df, tariff_index):
+    """ Appends "TARIFF_INDEX" on to config.csv along with its value.
+    """
     df2 = pd.DataFrame({df_column_one_name : ["TARIFF_INDEX"], df_column_two_name : [tariff_index]})
     df = pd.concat([df, df2], ignore_index = True, axis = 0)
     return df 
@@ -176,10 +209,6 @@ def main():
     except pd.errors.EmptyDataError:
         gridlabd.error(f"{config_file} file not found")
 
-    gridlabd.output(f"Success")
-
-
-
     try:
         is_column_names_valid(df)
         parse_csv_values(df,df_tariff_index)
@@ -187,7 +216,7 @@ def main():
     except ValueError as e:
         gridlabd.error(str(e))
 
-    
+    print(df.to_string())
     df.to_csv("config.csv", index = False)
 
 if __name__ == "__main__":
