568ceac6ac914a1b5204da0f3dcbad1646605b3b
Johnson Hsiung <johnson.hsiung@sjsu.edu>
2022-05-23 17:28:07 -0700

    add environment for recreation of multi meter seg fault

---
+++ autotest/default_billing.py
@@ -0,0 +1,3 @@
+def compute_bill(gridlabd, **kwargs):
+	#
+	return None
---
+++ autotest/setup.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+gridlabd template config set GITUSER slacgismo
+gridlabd template config set GITREPO gridlabd-template
+gridlabd template config set GITBRANCH add-tariff-design
+gridlabd template config set DATADIR /model/us/ca/slac
+gridlabd template get tariff_design
