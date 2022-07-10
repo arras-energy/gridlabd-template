a38c6bc2090eab4f8a0952348fa49cdd9d7cf28b
David P. Chassin <dchassin@slac.stanford.edu>
2022-07-10 09:13:54 -0700

    Add autotests (#18)

--- validate.sh
+++ validate.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
+
 set -x
 set -e
 
 for TEMPLATE in $(cat $2/.index); do
     GLMNAME=$(basename "$1")
     DIRNAME=${GLMNAME/.glm/}
-    TESTDIR=test/$(dirname "$1")/$DIRNAME/$2/$TEMPLATE
-    echo "Testing $TESTDIR/$GLMNAME..."
-    mkdir -p "$TESTDIR"
-    cp "$1" "$TESTDIR"
-    cd "$TESTDIR"
-    gridlabd template get "$TEMPLATE" 1>stdout 2>stderr
-    gridlabd "$GLMNAME" -t "$TEMPLATE" --redirect all 1>>stdout 2>>stderr
+    CHECKDIR="$2/$TEMPLATE/$(dirname "$1")/$DIRNAME"
+    if [ -d "$CHECKDIR" ]; then
+        TESTDIR=test/$CHECKDIR
+        mkdir -p "$TESTDIR"
+        cp "$1" "$TESTDIR"
+        cd "$TESTDIR"
+        gridlabd template get "$TEMPLATE" 1>stdout 2>stderr
+        gridlabd "$GLMNAME" -t "$TEMPLATE" --redirect all 1>>stdout 2>>stderr
+        ok="OK"
+        for FILE in $(find "$CHECKDIR" -type f -print); do
+            diff "$FILE" $(basename "$FILE") >> gridlabd.diff || ok="FAIL"
+        done
+        echo $1: $ok
+    fi
 done
