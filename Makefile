c7cf9fc5d927f5a73f68349dfe05a231787aadf8
David P. Chassin <david.chassin@me.com>
2022-07-10 11:06:07 -0700

    Fix autotest checks

--- Makefile
+++ Makefile
@@ -8,7 +8,12 @@ help:
 	@echo "  validate   perform validation tests"
 	@echo "Options:"
 	@echo "  LIMIT='timeout TIME'"
-	@echo "  DEBUG='--debug'"
+	@echo "  VERBOSE=yes"
+
+OPTIONS=
+ifeq ($(VERBOSE),yes)
+OPTIONS += --debug
+endif
 
 TEMPLATES=$(foreach ORG,$(shell grep -v ^\# .orgs),$(shell find $(ORG) -type d -print -prune))
 TESTDIR=autotest/models/gridlabd-4
@@ -21,4 +26,4 @@ clean:
 	@rm -rf test validate.txt
 
 $(TESTDIR)/%.glm: %.glm
-	@$(foreach TEMPLATE,$(TEMPLATES),$(LIMIT) ./validate.sh $(DEBUG) "$<" "$(TEMPLATE)") >> validate.txt
+	@$(foreach TEMPLATE,$(TEMPLATES),$(LIMIT) ./validate.sh $(OPTIONS) "$<" "$(TEMPLATE)") >> validate.txt
--- validate.sh
+++ validate.sh
@@ -2,28 +2,45 @@
 
 if [ $1 == "--debug" ]; then
     set -x
-    set -e
     shift 1
 fi
 
+ROOTDIR=$PWD
+result="OK"
 for TEMPLATE in $(cat $2/.index); do
     GLMNAME=$(basename "$1")
     DIRNAME=${GLMNAME/.glm/}
+    cd $ROOTDIR
     CHECKDIR="$2/$TEMPLATE/$(dirname "$1")/$DIRNAME"
     if [ -d "$CHECKDIR" ]; then
         TESTDIR=test/$CHECKDIR
-        mkdir -p "$TESTDIR"
+        mkdir -p "$ROOTDIR/$TESTDIR"
         cp "$1" $CHECKDIR/autotest.* "$TESTDIR"
-        cd "$TESTDIR"
+        cd "$ROOTDIR/$TESTDIR"
         AUTOTESTGLM=$(find . -name autotest.glm -type f -print)
         gridlabd template get "$TEMPLATE" 1>stdout 2>stderr
         gridlabd "$GLMNAME" $AUTOTESTGLM -t "$TEMPLATE" --redirect all 1>>stdout 2>>stderr
         ok="OK"
-        for FILE in $(find . -type f -print); do
-            if [ ! "$(basename $FILE)" == "autotest" ]; then
-                diff "$FILE" $(basename "$FILE") >> gridlabd.diff || ok="FAIL"
+        for FILE in $(find $ROOTDIR/$CHECKDIR -type f -print); do
+            TARGET=$(basename "$FILE")
+            if [ ! "${TARGET%.*}" == "autotest" ]; then
+                if [ ! -f "$TARGET" ]; then
+                    echo "ERROR: '$CHECKDIR/$TARGET' not found" > /dev/stderr
+                else
+                    diff -w "$FILE" "$TARGET" >> gridlabd.diff || ok="FAIL"
+                    if [ "$ok" == "FAIL" ]; then
+                        result="FAIL"
+                        echo "ERROR: '$CHECKDIR/$TARGET' is different" >/dev/stderr
+                    fi
+                fi
             fi
         done
         echo $1: $ok
     fi
 done
+
+if [ "$result" != "OK" ]; then
+    exit 1
+fi
+
+exit 0
