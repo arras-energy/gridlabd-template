f54e05e95fde7926d9ea2497037c627996df5f6b
David P. Chassin <david.chassin@me.com>
2022-07-10 10:14:30 -0700

    Add IEEE/13 ICA test

--- validate.sh
+++ validate.sh
@@ -10,13 +10,16 @@ for TEMPLATE in $(cat $2/.index); do
     if [ -d "$CHECKDIR" ]; then
         TESTDIR=test/$CHECKDIR
         mkdir -p "$TESTDIR"
-        cp "$1" "$TESTDIR"
+        cp "$1" $CHECKDIR/autotest.* "$TESTDIR"
         cd "$TESTDIR"
+        AUTOTESTGLM=$(find . -name autotest.glm -type f -print)
         gridlabd template get "$TEMPLATE" 1>stdout 2>stderr
-        gridlabd "$GLMNAME" -t "$TEMPLATE" --redirect all 1>>stdout 2>>stderr
+        gridlabd "$GLMNAME" $AUTOTESTGLM -t "$TEMPLATE" --redirect all 1>>stdout 2>>stderr
         ok="OK"
         for FILE in $(find "$CHECKDIR" -type f -print); do
-            diff "$FILE" $(basename "$FILE") >> gridlabd.diff || ok="FAIL"
+            if [ ! "$(basename $FILE)" == "autotest" ]; then
+                diff "$FILE" $(basename "$FILE") >> gridlabd.diff || ok="FAIL"
+            fi
         done
         echo $1: $ok
     fi
