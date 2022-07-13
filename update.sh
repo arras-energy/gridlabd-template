c53f61574adbb817f3a24753bdc3557f16602398
David P. Chassin <dchassin@slac.stanford.edu>
2022-07-13 07:31:11 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Syntax: update.sh TEMPLATE
+# Syntax: update.sh TEMPLATE [FOLDER]
 #
 # Update test result files in autotest/models folders of TEMPLATE
 #
@@ -10,8 +10,10 @@ if [ $# -lt 1 -o ! -d "$1/autotest" ]; then
 	exit 1
 fi
 ROOT=$PWD
+make models
+gridlabd template get $(basename $1)
 cd $1/autotest/models/gridlabd-4
-for DIR in $(find . -type d -print ); do
+for DIR in $(find . -type d -print | grep "$2" ); do
 	echo Processing $DIR...
 	if [ -f "$DIR/autotest.glm" ]; then
 		( cd $DIR ; gridlabd $ROOT/autotest/models/gridlabd-4/$DIR.glm autotest.glm -t $(basename $1) --redirect all )
