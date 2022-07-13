4e76c1688fcd32c30cf6a8ddb5e3dc61814550f1
David P. Chassin <david.chassin@me.com>
2022-07-13 05:46:02 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -14,7 +14,7 @@ cd $1/autotest/models/gridlabd-4
 for DIR in $(find . -type d -print ); do
 	echo Processing $DIR...
 	if [ -f "$DIR/autotest.glm" ]; then
-		( cd $DIR ; gridlabd $ROOT/autotest/models/gridlabd-4/$DIR.glm autotest.glm -t loadfactor )
+		( cd $DIR ; gridlabd $ROOT/autotest/models/gridlabd-4/$DIR.glm autotest.glm -t $(basename $1) )
 	fi
 done
 cd - >/dev/null
