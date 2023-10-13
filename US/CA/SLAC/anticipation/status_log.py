f03497729f2e9b9fcdfde35609c9d144a809fdca
aivanova5 <ivanova.alyona5@gmail.com>
2023-10-13 13:22:07 -0700

    Adding missing logging file

---
+++ US/CA/SLAC/anticipation/status_log.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import datetime
+
+pole_status = {} 
+metered_energy = {}
+data = {}
+objs = {}
+wrn_count = 0
+
+def get_info(t, object_class, target_property) : 
+    for obj in objs:
+        if gridlabd.get_object(obj)['class'] == object_class:
+            if t not in data:
+                data[t] = {}
+            data[t][obj] = gridlabd.get_object(obj)[target_property]
+    return data
+
+def dump_csv(data,file_name ) : 
+    # Flatten the nested dictionary
+    flat_data = []
+
+    for timestamp, item in data.items():
+        row = {'timestamp': datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')}
+        row.update(item)
+        flat_data.append(row)
+
+    # Create a DataFrame
+    df = pd.DataFrame(flat_data).set_index('timestamp')
+    df.to_csv(file_name, index=True)
+    return df
+
+def on_init(t) : 
+    global objs 
+    objs = gridlabd.get('objects')
+    return True
+
+def on_commit(t) :
+    global pole_status, wrn_count
+    pole_status.update(get_info(t,'pole', 'status'))
+    # pole_status.update(get_info(t,'pole', 'total_moment'))
+    try : 
+        metered_energy.update(get_info(t,'meter','measured_real_energy'))
+    finally : 
+        if wrn_count == 0 : 
+            print(f"WARNING [status_log]: meters are not available in the model.")
+            wrn_count = 1
+    return True 
+
+def on_term(t) : 
+    global data
+
+    df_energy = dump_csv(data,'./output/pole_status.csv')
+    return None
